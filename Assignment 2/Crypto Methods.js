993e2161bd706484bb3fa6482702f310fe2590c8
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2025-07-18 15:33:56 +0530

    Update Crypto Methods.js

--- Assignment 2/Crypto Methods.js
+++ Assignment 2/Crypto Methods.js
@@ -25,5 +25,16 @@ const hmac = crypto.createHmac('sha256', secret).update(message).digest('hex');
 console.log('HMAC:', hmac);
 
 
-const x=crypto.createDecipheriv('sha256', secret, "hello")
-console.log(x)
+const key = crypto.randomBytes(32); // For aes-256
+const iv = crypto.randomBytes(16); // 16-byte IV
+
+const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+let encrypted = cipher.update('Secret message', 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+decrypted += decipher.final('utf8');
+
+console.log('Encrypted:', encrypted);
+console.log('Decrypted:', decrypted);
